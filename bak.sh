e0d270d99824fb5e86f32b6b992b267323714369
Aaron Lichtman <aaronlichtman@gmail.com>
2020-01-07 17:21:05 +0100

    Spacing

--- bak.sh
+++ bak.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ $# -eq 1 ]; then
- cp -pvi "$1" "${1}.bak"
+    cp -pvi "$1" "${1}.bak"
 else
- echo "Info:  $0 copies to a backup file"
- echo "Usage: $0 <file to be backed up with .bak extension>"
+    echo "Info : $0 creates a local backup file (ending in .bak)"
+    echo "Usage: $0 [path to file to be backed up]"
 fi
