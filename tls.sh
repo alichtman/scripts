276fcabc6d39171e573e8a15ae9c02ef153dce95
Aaron Lichtman <aaronlichtman@gmail.com>
2020-01-08 23:47:30 +0100

    Formatting

--- tls.sh
+++ tls.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # $ tls -- tmux list sessions
 # Pretty print tmux session list.
-# Written by Aaron Lichtman (@alichtman on GitHub)
+# Written by Aaron Lichtman
+# https://github.com/alichtman/scripts/blob/master/tls.sh
 
 set -e;
 
@@ -10,7 +11,7 @@ BLUE="\033[94m"
 RED="\033[31m"
 RESET="\033[0m"
 
-echo -e "$BOLD$BLUE  == Active tmux Sessions ==$RESET";
+echo -e "\n$BOLD$BLUE  == Active tmux Sessions ==$RESET";
 
 sessions=$(tmux list-sessions | cut -d ":" -f1)
 for i in $sessions ; do
