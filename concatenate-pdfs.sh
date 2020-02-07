63629284ef2a24ec16f90d2bd0d23b7dae63ba6b
Aaron Lichtman <aaronlichtman@gmail.com>
2020-02-07 17:20:20 -0600

    Add concatenate-pdfs script

---
+++ concatenate-pdfs.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# USAGE: concatenate_pdfs INPUTS
+# Outputs the concatenated file as Merged.pdf
+
+if [ -z "$*" ]; then
+    echo "Error: No args.";
+    exit 1;
+fi
+OUTPUT_FILE="Merged.pdf"
+if [ -f "$OUTPUT_FILE" ]; then
+    # shellcheck disable=SC2162
+    # For some reason, read -r doesn't play well with macOS (GNU bash, version 5.0.11)
+	read -p "Warning: Merged.pdf will be overwritten. Continue? [y/N] " choice
+
+	case "$choice" in
+	  y|Y ) rm "$OUTPUT_FILE";;
+	  n|N|* ) echo "Aborting."; exit 1;;
+	esac
+fi
+
+gs -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE="$OUTPUT_FILE" -dPDFSETTINGS=/prepress -dBATCH ./*
+
