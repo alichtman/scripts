140a48164f1bcf3e803c0881ffb3f706bbc850df
Aaron Lichtman <aaronlichtman@gmail.com>
2020-04-30 07:07:46 -0500

    Update linux setup script

--- setup/linux-setup.sh
+++ setup/linux-setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-# This script does basic linux setup
+# This script does (highly opinionated) Linux setup on Debian-based distros.
+# Written by: Aaron Lichtman (@alichtman on GitHub)
+
+echo "Have you added contrib and non-free to the /etc/apt/sources.list file?"
+echo "    -> https://wiki.debian.org/SourcesList"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) echo "Continuing."; break;;
+        No ) exit;;
+    esac
+done
 
 sudo apt update
 
@@ -11,11 +21,11 @@ chsh -s "$(command -v zsh)"
 
 [ -d ~/.oh-my-zsh ] || sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "Maybe a good time to restart?"
-
-pip3 install shallow-backup>=4.0.1 pynvim thefuck
+pip3 install shallow-backup>=4.0.1
+pip3 install pynvim thefuck
 
 # shallow-backup + dotfiles
+# TODO: Convert to dotbot?
 mkdir ~/shallow-backup
 git clone https://github.com/alichtman/dotfiles ~/shallow-backup/dotfiles
 mv ~/shallow-backup/dotfiles/.config/shallow-backup.conf ~/.config
@@ -43,23 +53,34 @@ sudo apt install yq -y
 sudo apt install ctags ranger fzf silversearcher-ag libclang-dev hub xsel ddgr git-extras latte-dock polybar tmux -y
 
 # Gen new SSH key
-echo "Place this key at /home/alichtman/.ssh/alichtman-GitHub"
+echo "Place this key at ~/.ssh/alichtman-GitHub, and upload the public key to GitHub"
 ssh-keygen -t rsa -b 4096 -C "aaronlichtman@gmail.com"
+
+echo "Have you added the public key to GitHub?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) break;;
+        No ) break;;
+    esac
+done
 
 # Install lsd
 curl https://github.com/Peltoche/lsd/releases/download/0.16.0/lsd_0.16.0_amd64.deb -o ~/Downloads/lsd.deb
 sudo dpkg -i ~/Downloads/lsd.deb
+rm ~/Downloads/lsd.deb
 
 # tmux setup
 ln -s ~/.tmux/tmux.conf ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-echo "Prefix + I to install plugins"
+# Install tpm packages
+~/.tmux/plugins/tpm/bin/install_plugins
 
-# install diff-so-fancy
+# Install diff-so-fancy
 curl https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy -o ~/bin/diff-so-fancy
 chmod +x ~/bin/diff-so-fancy
 
 # Clone my repos
+# TODO: Move ~/Desktop/Development to ~/Development ?
 mkdir ~/Desktop/Development
 git clone --recursive git@github.com:alichtman/notes.git ~/Desktop/Development/notes
 git clone git@github.com:alichtman/writeups.git ~/Desktop/Development/writeups
@@ -68,20 +89,21 @@ git clone git@github.com:alichtman/fzf-notes.git ~/Desktop/Development/fzf-notes
 
 # Install my scripts
 mkdir ~/bin
-(cd ~/Desktop/Development/fzf-notes && chmod +x fzf-notes && ln -s $(realpath fzf-notes) ~/bin/fzf-notes)
-(cd ~/Desktop/Development/scripts && chmod +x tls.sh && ln -s $(realpath tls.sh) ~/bin/tls) 
+(cd ~/Desktop/Development/fzf-notes && chmod +x fzf-notes && ln -s "$(realpath fzf-notes)" ~/bin/fzf-notes)
+(cd ~/Desktop/Development/scripts && chmod +x tls.sh && ln -s "$(realpath tls.sh)" ~/bin/tls)
 
 # Install Hack Nerd font
-git clone --depth=1 git@github.com:ryanoasis/nerd-fonts.git /tmp/nerd-fonts
-(cd /tmp/nerd-fonts && ./install.sh Hack)
-rm -rf /tmp/nerd-fonts
-echo "WARN: Manual installation of Nerd Fonts required."
-xdg-open ~/.local/share/fonts/
+# git clone --depth=1 git@github.com:ryanoasis/nerd-fonts.git /tmp/nerd-fonts
+# (cd /tmp/nerd-fonts && ./install.sh Hack)
+# rm -rf /tmp/nerd-fonts
+# echo "WARN: Manual installation of Nerd Fonts required."
+# xdg-open ~/.local/share/fonts/
 
 # Install cargo
 curl https://sh.rustup.rs -sSf | sh
 
 cargo install ripgrep bat fd-find
 
-curl https://raw.githubusercontent.com/denilsonsa/prettyping/master/prettyping -o ~/bin/prettyping
-chmod +x ~/bin/prettyping
+curl https://raw.githubusercontent.com/denilsonsa/prettyping/master/prettyping -o ~/bin/prettyping && chmod +x ~/bin/prettyping
+
+echo -e "## Setup Complete"
