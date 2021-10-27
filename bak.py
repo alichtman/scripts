735c12b28ae6031c65f714c88db293585dcbd65c
Aaron Lichtman <aaronlichtman@gmail.com>
2021-10-27 02:20:00 -0500

    Add support for multiple files and folders in bak
    Fixes #3

---
+++ bak.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# This is a local backup tool. Handles files and directories while preserving the metadata.
+
+import os
+from sys import argv
+from glob import glob
+import shutil
+import argparse
+
+def main():
+	parser = argparse.ArgumentParser(description='Creates local backup files, ending in .bak')
+	parser.add_argument('files', metavar='file', type=str, nargs='+', help='files to be backed up. Accepts globs')
+	args = parser.parse_args()
+
+	globbed_list_of_files = []
+	for file in argv[1:]:
+		globbed = glob(file)
+		if globbed != []:
+			globbed_list_of_files.extend(globbed)
+
+	for file in globbed_list_of_files:
+		if file.endswith("/"):
+			directory = file
+			file = file[:-1]
+		bak_path = f"{file}.bak"
+		if os.path.isfile(file):
+			print(f"{file} -> {bak_path}")
+			shutil.copy2(file, bak_path)
+
+		else: # is directory
+			print(f"{directory} -> {bak_path}/")
+			shutil.copytree(directory, bak_path)
+
+
+
+if __name__ == "__main__":
+	main()
